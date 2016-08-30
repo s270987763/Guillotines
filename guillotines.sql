54f6274bd5528142c1702a6189331ffd3ec32858
MXi4oyu <MXi4oyu@hotmail.com>
2016-08-30 14:20:30 +0800

    初始化框架
    初始化框架

--- guillotines.sql
+++ guillotines.sql
@@ -1,71 +1,74 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50549
+Source Server         : mydb
+Source Server Version : 50540
 Source Host           : localhost:3306
 Source Database       : guillotines
 
 Target Server Type    : MYSQL
-Target Server Version : 50549
+Target Server Version : 50540
 File Encoding         : 65001
 
-Date: 2016-07-20 23:31:12
+Date: 2016-08-30 14:19:23
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for gu_shelldbconf
+-- Table structure for `gu_shelldbconf`
 -- ----------------------------
 DROP TABLE IF EXISTS `gu_shelldbconf`;
 CREATE TABLE `gu_shelldbconf` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `host` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `port` varchar(5) NOT NULL DEFAULT '3306',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `host` varchar(128) NOT NULL DEFAULT '127.0.0.1' COMMENT 'ipv4和ipv6',
+  `port` varchar(5) DEFAULT '3306' COMMENT '端口号',
+  `password` varchar(32) NOT NULL DEFAULT 'root' COMMENT '数据库密码',
   `create_time` timestamp NOT NULL DEFAULT '2016-07-20 23:30:27' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='websehll数据库配置信息';
 
 -- ----------------------------
 -- Records of gu_shelldbconf
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for gu_user
+-- Table structure for `gu_users`
 -- ----------------------------
-DROP TABLE IF EXISTS `gu_user`;
-CREATE TABLE `gu_user` (
-  `id` int(2) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(40) NOT NULL,
+DROP TABLE IF EXISTS `gu_users`;
+CREATE TABLE `gu_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL COMMENT '用户名',
+  `email` varchar(100) NOT NULL COMMENT '邮箱',
+  `password` varchar(32) NOT NULL COMMENT '密码',
   `salt` varchar(10) NOT NULL,
+  `utype` varchar(10) NOT NULL DEFAULT 'user' COMMENT '用户类型',
   `create_time` timestamp NOT NULL DEFAULT '2016-07-20 21:35:31' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
 -- ----------------------------
--- Records of gu_user
+-- Records of gu_users
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for gu_webshell
+-- Table structure for `gu_webshell`
 -- ----------------------------
 DROP TABLE IF EXISTS `gu_webshell`;
 CREATE TABLE `gu_webshell` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `ip` varchar(20) NOT NULL,
-  `ip_address` varchar(255) NOT NULL,
-  `category` varchar(6) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `ip` varchar(128) NOT NULL COMMENT 'IP地址',
+  `ip_address` varchar(255) NOT NULL COMMENT '归属地',
+  `category` varchar(6) NOT NULL COMMENT 'shell类型',
   `create_time` timestamp NOT NULL DEFAULT '2016-07-20 22:52:47' ON UPDATE CURRENT_TIMESTAMP,
+  `httpcode` varchar(3) DEFAULT NULL COMMENT '状态码',
+  `br` int(1) DEFAULT NULL COMMENT '权重',
+  `pr` int(1) DEFAULT NULL COMMENT 'PR',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of gu_webshell
 -- ----------------------------
-SET FOREIGN_KEY_CHECKS=1;
