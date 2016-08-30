2b0de18253eb1811063cabe16ffe3afd4c199b68
MXi4oyu <MXi4oyu@hotmail.com>
2016-08-30 16:16:32 +0800

    更新数据库表
    更新数据库表

--- guillotines.sql
+++ guillotines.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50540
 File Encoding         : 65001
 
-Date: 2016-08-30 14:19:23
+Date: 2016-08-30 16:16:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,12 +20,12 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `gu_shelldbconf`;
 CREATE TABLE `gu_shelldbconf` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `host` varchar(128) NOT NULL DEFAULT '127.0.0.1' COMMENT 'ipv4和ipv6',
-  `port` varchar(5) DEFAULT '3306' COMMENT '端口号',
-  `password` varchar(32) NOT NULL DEFAULT 'root' COMMENT '数据库密码',
-  `create_time` timestamp NOT NULL DEFAULT '2016-07-20 23:30:27' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `HOST` varchar(128) NOT NULL DEFAULT '127.0.0.1' COMMENT 'ipv4和ipv6',
+  `PORT` varchar(5) NOT NULL DEFAULT '3306' COMMENT '端口号',
+  `PWD` varchar(32) NOT NULL DEFAULT 'root' COMMENT '数据库密码',
+  `CREATE_TIME` timestamp NOT NULL DEFAULT '2016-07-20 23:30:27' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='websehll数据库配置信息';
 
 -- ----------------------------
@@ -37,14 +37,14 @@ CREATE TABLE `gu_shelldbconf` (
 -- ----------------------------
 DROP TABLE IF EXISTS `gu_users`;
 CREATE TABLE `gu_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL COMMENT '用户名',
-  `email` varchar(100) NOT NULL COMMENT '邮箱',
-  `password` varchar(32) NOT NULL COMMENT '密码',
-  `salt` varchar(10) NOT NULL,
-  `utype` varchar(10) NOT NULL DEFAULT 'user' COMMENT '用户类型',
-  `create_time` timestamp NOT NULL DEFAULT '2016-07-20 21:35:31' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(32) NOT NULL COMMENT '用户名',
+  `EMAIL` varchar(100) NOT NULL COMMENT '邮箱',
+  `PWD` varchar(32) NOT NULL COMMENT '密码',
+  `SALT` varchar(10) NOT NULL,
+  `UTYPE` varchar(10) NOT NULL DEFAULT 'user' COMMENT '用户类型',
+  `CREATE_TIME` timestamp NOT NULL DEFAULT '2016-07-20 21:35:31' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
 
 -- ----------------------------
@@ -56,17 +56,18 @@ CREATE TABLE `gu_users` (
 -- ----------------------------
 DROP TABLE IF EXISTS `gu_webshell`;
 CREATE TABLE `gu_webshell` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `ip` varchar(128) NOT NULL COMMENT 'IP地址',
-  `ip_address` varchar(255) NOT NULL COMMENT '归属地',
-  `category` varchar(6) NOT NULL COMMENT 'shell类型',
-  `create_time` timestamp NOT NULL DEFAULT '2016-07-20 22:52:47' ON UPDATE CURRENT_TIMESTAMP,
-  `httpcode` varchar(3) DEFAULT NULL COMMENT '状态码',
-  `br` int(1) DEFAULT NULL COMMENT '权重',
-  `pr` int(1) DEFAULT NULL COMMENT 'PR',
-  PRIMARY KEY (`id`)
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `URL` varchar(255) NOT NULL,
+  `PWD` varchar(32) NOT NULL,
+  `IP` varchar(128) NOT NULL COMMENT 'IP地址',
+  `IP_ADDRESS` varchar(255) NOT NULL COMMENT '归属地',
+  `CATEGORY` varchar(6) NOT NULL COMMENT 'shell类型',
+  `CREATE_TIME` timestamp NOT NULL DEFAULT '2016-07-20 22:52:47' ON UPDATE CURRENT_TIMESTAMP,
+  `HTTPCODE` varchar(3) DEFAULT NULL COMMENT '状态码',
+  `BR` int(1) DEFAULT NULL COMMENT '权重',
+  `PR` int(1) DEFAULT NULL COMMENT 'PR',
+  PRIMARY KEY (`ID`),
+  KEY `index_url` (`URL`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
